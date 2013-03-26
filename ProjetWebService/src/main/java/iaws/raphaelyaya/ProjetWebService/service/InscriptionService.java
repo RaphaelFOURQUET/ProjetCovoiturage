12bb83aa0827cb25197a066a8e139ec19e11d403
RaphaelFOURQUET <raphikifourque@gmail.com>
2013-03-26 15:59:53 +0100

    interface service inscription

--- ProjetWebService/src/main/java/iaws/raphaelyaya/ProjetWebService/service/InscriptionService.java
+++ ProjetWebService/src/main/java/iaws/raphaelyaya/ProjetWebService/service/InscriptionService.java
@@ -1,5 +1,9 @@
 package iaws.raphaelyaya.ProjetWebService.service;
 
+import iaws.raphaelyaya.ProjetWebService.data.Personne;
+
 public interface InscriptionService {
+	
+	public Personne inscription(Personne p);
 
 }
